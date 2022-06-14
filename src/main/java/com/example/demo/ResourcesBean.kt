fca4aaef9e4112d82cb1624e3f9ad7714b50bdea
Dan-Mikkel Hübenette <danmikkelhubenette@gmail.com>
2022-06-14 10:01:21 +0200

    Slang inn en ResourcesBean.kt

---
+++ src/main/java/com/example/demo/ResourcesBean.kt
@@ -0,0 +1,8 @@
+package com.example.demo
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.stereotype.Component
+
+@Component
+@ConfigurationProperties(prefix = "resources")
+data class ResourcesBean(var db: String = "", var logs: String = "", var secure: Boolean = true)
